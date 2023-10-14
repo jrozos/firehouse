6d47429ff05e75247dc3cc809fa34012be06b3bb
jrozos <sonarozos@gmail.com>
2023-10-14 15:29:00 -0600

    Se agrego error 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,22 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row my-5 mx-0 justify-content-center">
-  <div class="col-lg-10 col-md-8 mb-md-0 mb-4">
-    <div class="card">
-      <div class="card-header p-0 mx-3 mt-3 z-index-1">
-        <img src="{{asset('img/404.svg')}}" class="img-fluid border-radius-lg">
-      </div>
-    
-      <div class="card-body pt-2">
-        <span class="text-gradient text-primary text-uppercase text-xs font-weight-bold my-2">404</span>
-        <a href="javascript:;" class="card-title h5 d-block text-darker">
-          Página no encontrada
-        </a>
-        <p class="card-description mb-4">
-          Haz click en el boton <i class="fas fa-home text-info mb-0"></i> para regresar al Home
-        </p>
-      </div>
+<div class="container">
+  <div class="row justify-content-center pt-5">
+    <div class="col-12 col-md-6 col-lg-4 align-self-center">
+      <h1>Opps!</h1>
+      <h2>Página no econtrada</h2>
+      <p>Regresar a <a href="/" class="text-info">inicio</a></p>
+    </div>
+    <div class="col-12 col-md-6 col-lg-4">
+      <img src="{{asset('img/errors/404.svg')}}" alt="" class="img-fluid">
     </div>
   </div>
 </div>
